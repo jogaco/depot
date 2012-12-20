f46d7f6cabdf086b38cf4894d5490f1e219da48b
jogaco <jogaco.en@gmail.com>
2012-12-20 18:01:31 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '92296e441564f73f01b65ad56c4e84ec67c9a7401275620a91f2486560ec704e7b03f65a6d228c958a08636e1e28c661966fe10fd3ee726f5a32834c141a3077'
