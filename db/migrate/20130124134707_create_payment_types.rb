9a5f8d541ef7b279cd65382c97f985ea6ce669a1
jogaco <jogaco.en@gmail.com>
2013-02-13 15:39:52 +0100

    Iteration J3: translating checkout

--- db/migrate/20130124134707_create_payment_types.rb
+++ db/migrate/20130124134707_create_payment_types.rb
@@ -2,13 +2,12 @@ class CreatePaymentTypes < ActiveRecord::Migration
   def up
     create_table :payment_types do |t|
       t.string :name
-
       t.timestamps
     end
-    PaymentType.create(:name => 'Check')
-    PaymentType.create(:name => 'Credit Card')
-    PaymentType.create(:name => 'Purchase Order')
-    PaymentType.create(:name => 'Paypal')
+    PaymentType.create!(:name => 'Check')
+    PaymentType.create!(:name => 'Credit Card')
+    PaymentType.create!(:name => 'Purchase Order')
+    PaymentType.create!(:name => 'Paypal')
   end
 
   def down
