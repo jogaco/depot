4931714582fb69c872e991e4cd792841447ac865
jogaco <jogaco.en@gmail.com>
2013-02-13 11:14:12 +0100

    Playtime: added BASIC authentication for requests other than HTML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ class ApplicationController < ActionController::Base
 
   protected
     def authorize
-      unless User.find_by_id(session[:user_id])
-        redirect_to login_path, notice: 'Please log in'
+      if request.format == Mime::HTML
+        unless User.find_by_id(session[:user_id])
+          redirect_to login_path, notice: 'Please log in'
+        end
+      else
+        authenticate_or_request_with_http_basic('Administration') do |username, password|
+          user = User.find_by_name(username)
+          user and user.authenticate(password)
+        end
       end
     end
 
